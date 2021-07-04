c4ebb7695065dd1480b860ce6b96f751675b1c6e
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-04 03:35:15 +0300

    create image model

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category,on_delete= models.CASCADE)
+    location = models.ForeignKey(Location,on_delete= models.CASCADE)
+# on_delete= models.CASCADE : Telling django to delete the post once the author is deleted
