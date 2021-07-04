826392c331f9bfa9fc269e3fee26236ec022fabf
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-04 03:47:25 +0300

    test svaing,deleting and instance of the image class

--- albums/tests.py
+++ albums/tests.py
@@ -16,3 +16,19 @@ class TestImage(TestCase):
         Image.objects.all().delete()
         Location.objects.all().delete()
         Category.objects.all().delete()
+
+    # test instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))
+
+    # Test saving images
+    def test_save_image(self):
+        self.image_test.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    # test delete images
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
