d143ed629dcd397b61922e7101e3ff9a86732b43
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-04 04:07:59 +0300

    index view function,route and template working

--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http  import HttpResponse,Http404
+from .models import Location,Image
 
 # Create your views here.
+def index(request):
+    context ={
+        "images":Image.objects.all(),
+        "locations": Location.get_locations(),
+        "title": "Picture World"
+    }
+    return render(request, 'albums/index.html',context=context)
