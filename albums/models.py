46a64d66a20259d417661bb25c6fc827f4fd74e0
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-04 03:43:58 +0300

    cls methods for location class

--- albums/models.py
+++ albums/models.py
@@ -5,6 +5,24 @@ from django.utils import timezone
 class Location(models.Model):
     name = models.CharField(max_length=60)
 
+    @classmethod #applicable to the entire class. Not limited to the defined function
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
