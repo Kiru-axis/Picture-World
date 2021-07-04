1666583a3e24a552c231f531069622be4bdd6eaf
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-04 03:45:52 +0300

    running unittests for the image class

--- albums/tests.py
+++ albums/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import Image, Category, Location
 # Create your tests here.
+class TestImage(TestCase):
+    # setup method to inherit from
+    def setUp(self):
+        self.location = Location(name='Location')
+        self.location.save_location()
+
+        self.category = Category(name='food')
+        self.category.save_category()
+
+        self.image_test = Image(id=1, name='image', description='test image', location=self.location,category=self.category)
+    
